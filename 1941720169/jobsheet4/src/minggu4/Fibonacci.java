bd2ead0b4efb17c0fac8d1ade3aa9fbf1c592aac
DhimasArbi <dhimas.arbi@gmail.com>
2020-03-02 21:22:24 +0700

    update 02/03/2020
    Tugas 4

---
+++ 1941720169/jobsheet4/src/minggu4/Fibonacci.java
@@ -0,0 +1,21 @@
+package minggu4;
+
+public class Fibonacci {
+    public int angka;
+    
+    public void hitungFiboBF(int n){
+        int a=0,b=1;
+        for(int i=0;i<n;i++){
+            System.out.print(a+" ");
+            a=a+b;
+            b=a-b;
+        }
+    }
+    public int hitungFiboDC(int n){
+        if(n<=0 || n<=1){
+            return n;
+        }else{
+            return hitungFiboDC(n-2)+hitungFiboDC(n-1);
+        }
+    }
+}
