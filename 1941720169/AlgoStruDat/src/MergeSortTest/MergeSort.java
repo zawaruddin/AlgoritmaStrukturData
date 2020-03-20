f9e3bfcd45e76ada607421dae5bc69943833c6aa
DhimasArbi <dhimas.arbi@gmail.com>
2020-03-20 18:51:20 +0700

    Add Jobsheet 7

---
+++ 1941720169/AlgoStruDat/src/MergeSortTest/MergeSort.java
@@ -0,0 +1,51 @@
+package MergeSortTest;
+
+public class MergeSort {
+
+    public void mergeSort(int data[]) {
+        sort(data, 0, data.length - 1);
+    }
+
+    private void merge(int data[], int left, int middle, int right) {
+        int temp[] = new int[data.length];
+        for (int i = left; i <= right; i++) {
+            temp[i] = data[i];
+        }
+        int a = left;
+        int b = middle + 1;
+        int c = left;
+
+        while (a <= middle && b <= right) {
+            if (temp[a] < temp[b]) {
+                data[c] = temp[a];
+                a++;
+            } else {
+                data[c] = temp[b];
+                b++;
+            }
+            c++;
+        }
+        while(a<=middle){
+            data[c] = temp[a];
+            c++;
+            a++;
+        }
+    }
+
+    private void sort(int data[], int left, int right) {
+        if (left < right) {
+            int middle = (left + right) / 2;
+            sort(data, left, middle);
+            sort(data, middle + 1, right);
+            merge(data, left, middle, right);
+        }
+    }
+
+    public void printArray(int arr[]) {
+        int n = arr.length;
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println("");
+    }
+}
---
+++ 1941720169/AlgoStruDat/src/TestSearching/Searching.java
@@ -0,0 +1,94 @@
+package TestSearching;
+
+public class Searching {
+
+    public int data[];
+    public int jumData;
+
+    public Searching(int Data[], int jmlData) {
+        this.jumData = jmlData;
+        data = new int[jmlData];
+        for (int i = 0; i < jumData; i++) {
+            data[i] = Data[i];
+        }
+    }
+
+    public int FindSeqSearch(int cari) {
+        int posisi = -1;
+        for (int j = 0; j < jumData; j++) {
+            if (data[j] == cari) {
+                posisi = j;
+                break;
+            }
+        }
+        return posisi;
+    }
+
+    public void tampilData() {
+        for (int i = 0; i < jumData; i++) {
+            System.out.print(data[i] + " ");
+        }
+        System.out.println("");
+    }
+
+    public int FindBinarySearch(int cari, int left, int right) {
+        int mid;
+        if (right >= left) {
+            mid = (left + right) / 2;
+            if (cari == data[mid]) {
+                return (mid);
+            } else if (data[mid] > cari) {
+                return FindBinarySearch(cari, left, mid-1);
+            } else{
+                return FindBinarySearch(cari, mid + 1, right);
+            }
+        }
+        return -1;
+    }
+    public void mergeSort() {
+        sort(data, 0, data.length - 1);
+    }
+
+    private void merge(int Data[], int left, int middle, int right) {
+        int temp[] = new int[Data.length];
+        for (int i = left; i <= right; i++) {
+            temp[i] = Data[i];
+        }
+        int a = left;
+        int b = middle + 1;
+        int c = left;
+
+        while (a <= middle && b <= right) {
+            if (temp[a] < temp[b]) {
+                Data[c] = temp[a];
+                a++;
+            } else {
+                Data[c] = temp[b];
+                b++;
+            }
+            c++;
+        }
+        while(a<=middle){
+            Data[c] = temp[a];
+            c++;
+            a++;
+        }
+    }
+
+    private void sort(int Data[], int left, int right) {
+        if (left < right) {
+            int middle = (left + right) / 2;
+            sort(Data, left, middle);
+            sort(Data, middle + 1, right);
+            merge(Data, left, middle, right);
+        }
+    }
+
+    public void TampilPosisi(int x, int pos) {
+        if (pos != -1) {
+            System.out.println("data: " +x+" ditemukan pada indeks "+ pos);
+        }else{
+            System.out.println("data: " +x+" tidak ditemukan");
+        }
+    }
+}
