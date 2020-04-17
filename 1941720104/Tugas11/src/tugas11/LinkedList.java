b5a3856124650b6e776446921e2854d70f6e6e1f
Daffa Cahyo Alghifari <ndap.cahyo@gmail.com>
2020-04-17 23:15:51 +0700

    Kodingan Pertemuan 11

---
+++ 1941720104/Tugas11/src/tugas11/LinkedList.java
@@ -0,0 +1,142 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tugas11;
+
+/**
+ *
+ * @author Daffa Cahyo
+ */
+public class LinkedList {
+
+    Node head;
+    int size;
+
+    public LinkedList() {
+        head = null;
+        size = 0;
+    }
+
+    public boolean isEmpty() {
+        return head == null;
+    }
+
+    public void addFirst(int item) {
+        head = new Node(item, head);
+        size++;
+    }
+
+    public void add(int item, int index) throws Exception {
+        if (index < 0 || index > size) {
+            throw new Exception("Nilai index di luar batas");
+        }
+        if (isEmpty() || index == 0) {
+            addFirst(item);
+        } else {
+            Node tmp = head;
+            for (int i = 1; i < index; i++) {
+                tmp = tmp.next;
+            }
+            Node next = (tmp == null) ? null : tmp.next;
+            tmp.next = new Node(item, next);
+        }
+        size++;
+    }
+
+    public void addLast(int item) {
+        if (isEmpty()) {
+            addFirst(item);
+        } else {
+            Node tmp = head;
+            while (tmp.next != null) {
+                tmp = tmp.next;
+            }
+            tmp.next = new Node(item, null);
+        }
+        size++;
+    }
+
+    public int getFirst() throws Exception {
+        if (isEmpty()) {
+            throw new Exception("LinkedList kosong");
+        }
+        return head.data;
+    }
+
+    public int getLast() throws Exception {
+        if (isEmpty()) {
+            throw new Exception("LinkedList kosong");
+        }
+        Node tmp = head;
+        while (tmp.next != null) {
+            tmp = tmp.next;
+        }
+        return tmp.data;
+    }
+
+    public int get(int index) throws Exception {
+        if (isEmpty() || index >= size) {
+            throw new Exception("Nilai index di luar batas");
+        }
+        Node tmp = head;
+        for (int i = 0; i < index; i++) {
+            tmp = tmp.next;
+        }
+        return tmp.data;
+    }
+
+    public void remove(int index) throws Exception {
+        if (isEmpty() || index >= size) {
+            throw new Exception("Nilai index di luar batas");
+        } else if (index == 0) {
+            removeFirst();
+        } else {
+            Node prev = head;
+            Node cur = head.next;
+            for (int i = 1; i < index; i++) {
+                prev = cur;
+                cur = prev.next;
+            }
+            size--;
+        }
+    }
+
+    public void removeFirst() throws Exception {
+        head = head.next;
+        size--;
+    }
+
+    public void removeByValue(int item, int index, int cari) throws Exception {
+        if (cari == item) {
+            remove(index);
+        } else {
+            System.out.println("Linkedlist Kosong");
+        }
+    }
+    
+    public void addByValue (int index, int item, int cr) throws Exception{
+        if(cr == item){
+            add(item, index);
+        }
+    }
+
+    public void clear() {
+        head = null;
+        size = 0;
+    }
+
+    public void print() {
+        if (!isEmpty()) {
+            Node tmp = head;
+            while (tmp != null) {
+                System.out.println(tmp.data + "\t");
+                tmp = tmp.next;
+            }
+            System.out.println();
+        } else {
+            System.out.println("LinkedList kosong");
+        }
+    }
+}
---
+++ 1941720104/Tugas11/src/tugas11/MainLinkedList.java
@@ -0,0 +1,150 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tugas11;
+
+/**
+ *
+ * @author Daffa Cahyo
+ */
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class MainLinkedList {
+
+    static Scanner daffa = new Scanner(System.in);
+
+    public static int jawaban, i;
+
+    public static void main(String[] args) {
+        LinkedList data = new LinkedList();
+
+        System.out.println("**********MENU**********");
+        System.out.println("1. Tambah");
+        System.out.println("2. Hapus");
+        System.out.println("3. Cari");
+        System.out.println("4. Keluar");
+        System.out.println("************************");
+        System.out.print("Jawaban: ");
+        jawaban = daffa.nextInt();
+
+        switch (jawaban) {
+            case 1:
+                Tambah();
+                break;
+            case 2:
+                Hapus();
+                break;
+            case 3:
+                Cari();
+                break;
+            default:
+        }
+    }
+
+    public static void Tambah() {
+        LinkedList data = new LinkedList();
+        int nilai, cari;
+        System.out.println("1. Tambah (Pertama)");
+        System.out.println("2. Tambah (Index)");
+        System.out.println("3. Tambah (Terakhir");
+        System.out.print("Jawaban: ");
+        jawaban = daffa.nextInt();
+
+        switch (jawaban) {
+            case 1:
+                System.out.print("Masukkan angka yang ingin ditambahkan: ");
+                i = daffa.nextInt();
+                data.addFirst(i);
+                data.print();
+                break;
+            case 2:
+                System.out.print("Masukkan angka yang ingin ditambahkan: ");
+                i = daffa.nextInt();
+                System.out.print("Masukkan nilai: ");
+                nilai = daffa.nextInt();
+                System.out.print("Masukkan cari: ");
+                cari = daffa.nextInt();
+                 {
+                    try {
+                        data.addByValue(i, nilai, cari);
+                    } catch (Exception ex) {
+                        Logger.getLogger(MainLinkedList.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                }
+                data.print();
+                break;
+            case 3:
+                System.out.print("Masukkan angka yang ingin ditambahkan: ");
+                i = daffa.nextInt();
+                System.out.print("Masukkan nilai: ");
+                nilai = daffa.nextInt();
+                 {
+                    try {
+                        data.add(nilai, i);
+                    } catch (Exception ex) {
+                        Logger.getLogger(MainLinkedList.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                }
+                data.print();
+                break;
+        }
+    }
+
+    public static void Hapus() {
+        int nilai, cari;
+        LinkedList data = new LinkedList();
+        System.out.println("1. Hapus (Index)");
+        System.out.println("2. Hapus (Key)");
+        System.out.println("3. Clear");
+        System.out.print("Jawaban: ");
+        jawaban = daffa.nextInt();
+
+        switch (jawaban) {
+            case 1:
+                System.out.println("Masukkan angka yang ingin dihapus: ");
+                i = daffa.nextInt();
+                System.out.print("Masukkan nilai: ");
+                nilai = daffa.nextInt();
+                System.out.print("Masukkan cari: ");
+                cari = daffa.nextInt();
+                 {
+                    try {
+                        data.removeByValue(i, nilai, cari);
+                    } catch (Exception ex) {
+                        Logger.getLogger(MainLinkedList.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                }
+                data.print();
+                break;
+            case 2:
+                System.out.println("Masukkan angka yang ingin dihapus: ");
+                i = daffa.nextInt();
+                {
+                     try {
+                        data.remove(i);
+                    } catch (Exception ex) {
+                        Logger.getLogger(MainLinkedList.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                }
+                data.print();
+                break;
+            case 3:
+                data.clear();
+                data.print();
+                break;
+            default:
+        }
+    }
+    
+    public static void Cari(){
+        LinkedList data = new LinkedList();
+        System.out.println("1. Cari (Index)");
+        System.out.println("2. Cari (Key)");
+        System.out.print("Jawaban: ");
+        jawaban = daffa.nextInt();
+    }
+}
